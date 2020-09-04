6b611ef25c0719e78e89d91d5c233845c52b6e9b
Dinh Van Dien <democf2000@gmail.com>
2020-09-04 10:11:40 +0700

    add index

--- Dog.js
+++ Dog.js
@@ -1,3 +1,9 @@
 function Dog() {
 	// body...
+	this.stomach = []
 }
+Dog.prototype.eat = function (cats) {
+	// body...
+	stomach.push(cats)
+}
+module.exports = Dog
---
+++ index.js
@@ -0,0 +1,6 @@
+var Dog = require('./Dog');
+var Cat = require('./Cat');
+ var dog = new Dog('Tom');
+ var meo = new Cat();
+meo.eat(dog);
+console.log(meo)
